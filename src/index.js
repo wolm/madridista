ac011caca84b66d2b9c2c350cf51bbbc1bd8b023
Torben Wölm <torben.wolm@lego.com>
2020-03-06 16:47:43 +0100

    :fire: Removed redundant component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import Sofabold from './components/sofabold';
+import React from "react"
+import { useActiveMatches } from './infrastructure/data-provider';
+import { matchMapper } from './infrastructure/match-mapper';
+import SofaboldContainer from "./components/sofabold-container";
+
+const Sofabold = (props) => {
+    const matches = useActiveMatches(props.dataUrl, matchMapper);
+
+    return (<SofaboldContainer matches={ matches } />);
+}
 
 export default Sofabold;
